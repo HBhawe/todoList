a7ac20c64d28cb839687f3e82f443c020088f828
HBhawe <harshalb97@outlook.com>
2025-03-25 16:29:41 +0100

    Cleaning up files and adding date-fns package

--- src/pageLoad.js
+++
@@ -1,6 +0,0 @@
-export const pageLoad = function (content) {
-  const heading = `<h1>Welcome to my restaurant</h1>`;
-  const text = `<p>This restaurant has been open for 25 years!`;
-  content.insertAdjacentHTML("beforeend", heading);
-  content.insertAdjacentHTML("beforeend", text);
-};
